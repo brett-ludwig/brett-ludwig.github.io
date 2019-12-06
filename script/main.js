73f7a79404a7759c894d53acb39903f4c4d1bf6c
Brett <brett.a.ludwig@gmail.com>
2019-12-06 03:58:25 -0500

    sound changes

--- script/main.js
+++ script/main.js
@@ -5,15 +5,23 @@ function startup() {
     bgm.play()
 }
 
+function softRain() {
+  bgm = new Audio('./res/rain.mp3');
+  bgm.play();
+}
+
 function dropNuke() {
   if (!nukeDropped){
     var snd = new Audio("./res/nukeDrop.mp3");
     document.getElementById("warzone").src = "https://image.flaticon.com/icons/svg/2070/2070300.svg";
+    bgm.pause();
     snd.play();
     setTimeout(
       function () {
       document.getElementById("warzone").src = "https://cdn3.iconfinder.com/data/icons/modern-war/514/war-001-512.png";
     }, 13000);
+
+    setTimeout( softRain(), 3000);
 
     nukeDropped = true;
   }
